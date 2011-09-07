2bc6c226ba84c09a1d5172c7d87864162997924a
Tomen <tommienenkel@hotmail.com>
2011-09-07 18:03:07 +0200

    creating welcome page

--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -10,7 +10,7 @@
 <html>
 <head>
 	<?php echo $this->html->charset();?>
-	<title>Application > <?php echo $this->title(); ?></title>
+	<title>Elenears Erben > <?php echo $this->title(); ?></title>
 	<?php echo $this->html->style(array('debug', 'lithium')); ?>
 	<?php echo $this->scripts(); ?>
 	<?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
@@ -18,9 +18,9 @@
 <body class="app">
 	<div id="container">
 		<div id="header">
-			<h1>Application</h1>
+			<h1>Elenears Erben</h1>
 			<h2>
-				Powered by <?php echo $this->html->link('Lithium', 'http://lithify.me/'); ?>.
+				Wir tragen das Licht weiter.
 			</h2>
 		</div>
 		<div id="content">
