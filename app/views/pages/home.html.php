d064ebae9bd67949ffb1c545a4727653e03684c1
Tomen <tommienenkel@hotmail.com>
2011-09-07 18:26:43 +0200

    adding account mvc

--- app/views/pages/home.html.php
+++ app/views/pages/home.html.php
@@ -14,29 +14,27 @@ $this->title('Willkommen');
 $self = $this;
 ?>
 <h3>Willkommen!</h3>
-<ul>
-	<li>
+	<p>
 		Die Kisten waren verstaut, das Vieh versperrt, draußen brannte die Welt. Die Götter hatten ihr Werk getan, und setzen sich nun wohlverdient zur Ruhe. Elenear loderte auf und ward sobald verbrannt.
-	</li>
-	<li>
+	</p>
+	<p>
 		Die meißten blieben.
-	</li>
-	<li>
+	</p>
+	<p>
 		Doch eine Handvoll Männer und Frauen machte sich auf den Weg. Auf den Weg ins Nichts. Die Welt hinter uns brannte und so konnten wir nur unsere Schiffe beladen, losfahren... und... hoffen. Hoffen, dass wir irgendwann auf Land stoßen würden, oder auf offener See verhungern oder gar... verdursten. Elenear liegt im sterben und das einzige was davon noch übrig ist, sind wir...	
-	</li>
-	<li>
+	</p>
+	<p>
 		Elenears Erben
-	</li>
-		<li>
+	</p>
+	<p>
 		<?php echo $self->html->link('Registrieren', array('controller' => 'Account', 'action' => 'create')); ?>
-	</li>
-	<li>
+	</p>
+	<p>
 		<?php echo $self->html->link('Einloggen', array('controller' => 'Account', 'action' => 'login')); ?>
-	</li>
-	<li>
+	</p>
+	<p>
 		<?php echo $this->html->link('Forum', 'http://elenear.net/phpbb'); ?>
-	</li>
-	<li>
+	</p>
+	<p>
 		<?php echo $this->html->link('News', 'http://elenear.net/wordpress'); ?>.
-	</li>
-</ul>
+	</p>
