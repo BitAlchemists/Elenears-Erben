61f11aa522c99ec513264e6dabed3fe62699be95
Tomen <tommienenkel@hotmail.com>
2011-09-08 15:50:20 +0200

    accounts

--- app/controllers/AccountsController.php
+++ app/controllers/AccountsController.php
@@ -34,6 +34,7 @@ class AccountsController extends \lithium\action\Controller {
 		
 			//$this->request->data
 			$username = $this->request->data->username;
+			var_dump($this->request->data);
 			echo "username: ".$username."<br/>";
 			$accounts = Accounts::all(array('conditions' => array('username' => true), 'limit' => 10));
 			var_dump($accounts->to());
