3e02cc728db6b8020c2ab3ab5da766d350038ef7
Tomen <tommienenkel@hotmail.com>
2011-09-08 13:47:38 +0200

    adding impressum

---
+++ app/views/pages/impressum.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2011, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+use lithium\core\Libraries;
+use lithium\data\Connections;
+
+$this->title('Impressum');
+
+$self = $this;
+?>
+<h3>Impressum</h3>
+	<p>
+		Thomas Enenkel<br/>
+		Steudelgasse 28/28<br/>
+		1100 Wien<br/>
+		E-Mail: tomen (ät) elenear (dot) net
+	</p>
