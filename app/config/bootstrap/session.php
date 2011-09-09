9808d88f794942d8ac6ac5b52bde86a828daf1b1
Tomen <tommienenkel@hotmail.com>
2011-09-09 19:18:30 +0200

    authentication

--- app/config/bootstrap/session.php
+++ app/config/bootstrap/session.php
@@ -61,7 +61,7 @@ Dispatcher::applyFilter('_callable', function($self, $params, $chain) {
     }
     return function() {
         //return new Response(array('controller' => 'pages', 'action' => 'home'));
-		return new Response(array('location' => '/game/pages/home'));
+		return new Response(array('location' => '/game/pages/home')); //TE: dont know why it works no other way for now =(
     };
 });
 
