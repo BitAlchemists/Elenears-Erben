b4677d9c11190036b965feaca3ebf364ca439269
Tomen <tommienenkel@hotmail.com>
2011-09-12 19:56:45 +0200

    adding gamescontroller

--- app/controllers/GamesController.php
+++ app/controllers/GamesController.php
@@ -114,7 +114,7 @@ class GamesController extends \lithium\action\Controller {
 		}
 		$game = Games::first(array('conditions' => array('_id' => $gameId)));
 		
-		foreach($game->avatars in $avatar)
+		foreach($game->avatars as $avatar)
 		{
 			if($avatar->userId == $userId)
 			{
