38e4f4b81eff5feeed1b14dcef22201808a8ba30
Tomen <tommienenkel@hotmail.com>
2011-09-12 18:18:23 +0200

    merging SessionsController.php into UsersController.php

--- app/views/users/add.html.php
+++
@@ -1,5 +0,0 @@
-<?=$this->form->create(null); ?>
-    <?=$this->form->field('username'); ?>
-    <?=$this->form->field('password', array('type' => 'password')); ?>
-    <?=$this->form->submit('Log in'); ?>
-<?=$this->form->end(); ?>
--- app/views/users/login.html.php
+++ app/views/users/login.html.php
@@ -1,1 +1,5 @@
-﻿This is the login page
+﻿<?=$this->form->create(null); ?>
+    <?=$this->form->field('username'); ?>
+    <?=$this->form->field('password', array('type' => 'password')); ?>
+    <?=$this->form->submit('Log in'); ?>
+<?=$this->form->end(); ?>
