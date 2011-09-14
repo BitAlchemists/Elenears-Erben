5345cd3d243f5cef5f6d5b4a44f69ab50531fe0a
Tomen <tommienenkel@hotmail.com>
2011-09-14 12:28:31 +0200

    adding tick console command

---
+++ app/extensions/command/Tick.php
@@ -0,0 +1,20 @@
+﻿<?php
+
+namespace app\extensions\command;
+
+/**
+ * The EE Heart
+ */
+class Tick extends \lithium\console\Command {
+
+	/**
+	 * The main method of the command.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		return $this->out('ticking');
+	}
+}
+
+?>
