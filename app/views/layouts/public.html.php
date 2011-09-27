bb1d7bcc72c0b887f35874060faa742ccd8f30b0
Tomen <tommienenkel@hotmail.com>
2011-09-27 15:33:02 +0200

    restructuring public landing page

---
+++ app/views/layouts/public.html.php
@@ -0,0 +1,36 @@
+﻿<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2011, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+?>
+<!doctype html>
+<html>
+<head>
+	<?php echo $this->html->charset();?>
+	<title>Elenears Erben > <?php echo $this->title(); ?></title>
+	<?php echo $this->html->style(array('debug', 'lithium')); ?>
+	<?php echo $this->scripts(); ?>
+	<?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
+</head>
+<body class="app">
+	<div id="container">
+		<div id="navigation">
+			<ul class="top-nav">
+				<li><?php echo $this->html->link('Home', '/'); ?></li>
+			</ul>
+		</div>
+		<div id="header">
+			<h1><?php echo $this->html->link('Elenears Erben', '/'); ?></h1>
+			<h2>
+				Wir tragen das Licht weiter.
+			</h2>
+		</div>
+		<div id="content">
+			<?php echo $this->content(); ?>
+		</div>
+	</div>
+</body>
+</html>
