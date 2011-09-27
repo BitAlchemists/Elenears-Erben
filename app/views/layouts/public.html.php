0f67f89ba4bf92e17369c2f7f64d4c79bd87d0b1
Tomen <tommienenkel@hotmail.com>
2011-09-27 16:55:03 +0200

    restructuring public landing page

--- app/views/layouts/public.html.php
+++ app/views/layouts/public.html.php
@@ -18,40 +18,40 @@ use lithium\security\Auth;
 	<?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
 </head>
 <body class="app">
-	<div id="container">
-		<div id="top-nav">
-			<div id="top-nav-left">
-				<ul>
-					<li><?php echo $this->html->link('Home', '/'); ?></li>
-					<li><?php echo $this->html->link('Impressum', array('controller' => 'pages', 'action' => 'impressum')); ?></li>
-					<li><?php echo $this->html->link('Forum', 'http://elenear.net/phpbb'); ?></li>		
-				</ul>
-			</div>
-			<div id="top-nav-right">
-				<ul>
-					<li>	
-						<?php 
-							if(!Auth::check('default'))
-							{
-								echo $this->html->link('Registrieren', array('controller' => 'users', 'action' => 'create')); 
-							}
-						?>
-					</li>
-					<li>
-						<?php 
-							if(Auth::check('default'))
-							{
-								echo $this->html->link('Ausloggen', array('controller' => 'users', 'action' => 'logout'));
-							}
-							else
-							{
-								echo $this->html->link('Einloggen', array('controller' => 'users', 'action' => 'login'));
-							}
-						?>
-					</li>
-				</ul>
-			</div>
+	<div id="top-nav">
+		<div id="top-nav-left">
+			<ul>
+				<li><?php echo $this->html->link('Home', '/'); ?></li>
+				<li><?php echo $this->html->link('Impressum', array('controller' => 'pages', 'action' => 'impressum')); ?></li>
+				<li><?php echo $this->html->link('Forum', 'http://elenear.net/phpbb'); ?></li>		
+			</ul>
 		</div>
+		<div id="top-nav-right">
+			<ul>
+				<li>	
+					<?php 
+						if(!Auth::check('default'))
+						{
+							echo $this->html->link('Registrieren', array('controller' => 'users', 'action' => 'create')); 
+						}
+					?>
+				</li>
+				<li>
+					<?php 
+						if(Auth::check('default'))
+						{
+							echo $this->html->link('Ausloggen', array('controller' => 'users', 'action' => 'logout'));
+						}
+						else
+						{
+							echo $this->html->link('Einloggen', array('controller' => 'users', 'action' => 'login'));
+						}
+					?>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div id="container">
 		<div id="header">
 			<h1><?php echo $this->html->link('Elenears Erben', '/'); ?></h1>
 			<h2>Wir tragen das Licht weiter.</h2>
