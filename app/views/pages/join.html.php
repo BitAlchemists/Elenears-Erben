5ddf11aa33f8c93435d1fce5c0b69fe0d1b098ee
Tomen <tommienenkel@hotmail.com>
2011-09-27 19:33:50 +0200

    restructuring public landing page

---
+++ app/views/pages/join.html.php
@@ -0,0 +1,41 @@
+﻿<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2011, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+use lithium\core\Libraries;
+use lithium\data\Connections;
+use lithium\storage\Session;
+use lithium\security\Auth;
+use lithium\util\String;
+
+
+$this->title('Mach mit!');
+
+?>
+<div>
+	<h3>Mach mit!</h3>
+	<p>
+		Elenears Erben ist ein Gemeinschaftsprojekt. Es gibt an vielen Orten etwas zu tun! Für Fragen und Bewerbungen, schreib eine Mail an tomen(ät)elenear.net
+	</p>
+	<h4>Wünsch Dir was =)</h4>
+	<p>
+		Dieses Spiel entsteht aus der Community heraus. Das heißt, dass jeder sich wünschen kann, in welche Richtung sich das Spiel entwickeln soll. Aus dieser gemeinsamen Vision entsteht das Spiel. Erzähl den anderen einfach von Deiner Idee!
+	</p>
+	<h4>Tester gesucht</h4>
+	<p>
+		Es gibt laufend neue Updates auf der Website. Informier Dich, welche Features gerade neu sind und teste sie ausgiebig! Deine Eindrücke, Feedback und Fehlerbeschreibungen helfen, das Spiel zu verbessern. Selbst solche "Kleinigkeiten" wie Verbesserungsideen zum Design helfen enorm.
+	</p>
+	<h4>Künstler gesucht</h4>
+	<p>
+		Elenears Erben soll vor allem ein visuelles Spiel werden. Dafür brauchen wir Assets. Begabte Künstler stehen momentan ganz oben auf unserer Gesucht!-Liste.
+	</p>
+	<h4>Entwickler gesucht</h4>
+	<p>
+		Fürs Front- und Backend können wir logischerweise immer noch mehr helfende Hände gebrauchen. Egal wie gut Deine Fähigkeiten in HTML und PHP sind, wir finden wir jeden Entwicklungslevel eine spannende Aufgabe.
+	</p>
+</div>
+
