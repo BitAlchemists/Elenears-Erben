ca3fa7cc69724257d346a5ebb8b6ecac760a04cd
Tommi Enenkel <tomen@elenear.net>
2012-01-11 15:56:01 +0100

    closing in on #60

--- app/views/games/view.html.php
+++ app/views/games/view.html.php
@@ -21,24 +21,71 @@ Euer Kartenzeichner hat Euch die neueste Karte der Welt schicken lassen:<br/>
 <?php
 	$this->scripts($this->html->script('jquery-1.4.2.js'));
 	$this->scripts($this->html->script('jquery.event.drag-2.0.js'));
+	$this->scripts($this->html->script('caat.js'));
 	$this->scripts($this->html->script('map.js'));
 ?>
-		<script type="text/javascript"> 
-			var map;
-			jQuery(document).ready(function(){
-				map = new map2DFramework( document.getElementById('map') );
-				map.loadMap(
-					{ 
-						"xSize":10,
-						"ySize":10,
-						"data":<?php echo($map); ?>,
-						"units":<?php echo($visibleUnits); ?>
-					}	
-				);
-				map.test();
-				setTimeout("map.drawMap()",500); //we redraw after a half second to be sure, that images are already loadedsetTimeout("map.drawMap()",500); //we redraw after a half second to be sure, that images are already loaded
+	<script type="text/javascript"> 
+		var map1;
 
-			})
+		var mapData = { 
+			"xSize":10,
+			"ySize":10,
+			"fields":<?php echo($map); ?>,
+			"units":<?php echo($visibleUnits); ?>
+		};
+
+		/**
+		* This function will be called to let you define new scenes that will be
+		* shown after the splash screen.
+		* @param director
+		*/
+		function createScenes(director) {
+
+              	var scene=              director.createScene();
+			var mapRenderer = new MapRenderer(director);
+			mapRenderer.loadMap(mapData);
+			mapRenderer.drawMap(scene);
+		};
+
+		jQuery(document).ready(function(){
+
+			CAAT.modules.initialization.init(
+				/* canvas will be 800x600 pixels */
+				700, 500,
+
+				/* and will be added to the end of document. set an id of a canvas or div element */
+				'map',
+				/*
+				 load these images and set them up for non splash scenes.
+				 image elements must be of the form:
+				 {id:'<unique string id>',    url:'<url to image>'}
+
+				 No images can be set too.
+				 */
+				[
+					{'id':'grasland', 	'url': EE.basePaths.image + 'field_grasland.png'},
+					{'id':'water', 	'url': EE.basePaths.image + 'field_water.png'},
+					{'id':'hunters',	'url': EE.basePaths.image + 'units/baddie_Ninja.png'}
+				],
+
+				/*
+				 onEndSplash callback function.
+				 Create your scenes on this method.
+				*/
+				createScenes
+			);
+
+/*
+				map1 = new map2DFramework( document.getElementById('map') );
+				map1.loadMap(mapData);
+				map1.test();
+				setTimeout("map1.drawMap()",500); //we redraw after a half second to be sure, that images are already loadedsetTimeout("map.drawMap()",500); //we redraw after a half second to be sure, that images are already loaded
+*/
+
+
+			});
+
+
 		</script> 
 <!--		TE: Manas Umrahmung für die Map
 <style type="text/css"> 
@@ -53,9 +100,9 @@ Euer Kartenzeichner hat Euch die neueste Karte der Welt schicken lassen:<br/>
 			}
 		</style>  -->
 
-		<button onclick="map.drawMap();">Karte zeigen</button>
+		<button onclick="map1.drawMap();">Karte zeigen</button>
 		<br/> 
-		<div id="map" style="width: 200px; height: 200px;"></div> 
+		<div id="map" style="width: 800; height: 600px;"></div> 
 <h3>Avatare:</h3>
 <?php
 	foreach($game->avatars as $avatar)
--- app/webroot/js/map.js
+++ app/webroot/js/map.js
@@ -7,6 +7,186 @@
  * @author        Daniel Fahlke
  */
 
+/**
+ * @license
+ * 
+ * The MIT License
+ * Copyright (c) 2010-2011 Ibon Tolosana, Hyperandroid || http://labs.hyperandroid.com/
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+// CAAT bootstrap
+
+CAAT.modules.initialization= CAAT.modules.initialization || {};
+
+
+CAAT.modules.initialization.init= function( width, height, runHere, imagesURL, onEndLoading )   {
+
+    /**
+     * infere whether runhere is on a DIV, canvas, or none at all.
+     * If none at all, just append the created canvas to the document.
+     */
+    var isCanvas= false;
+    var canvascontainer= document.getElementById(runHere);
+
+    if ( canvascontainer ) {
+        if ( canvascontainer instanceof HTMLDivElement ) {
+            isCanvas= false;
+        } else if ( canvascontainer instanceof HTMLCanvasElement ) {
+            isCanvas= true;
+        } else {
+            canvascontainer= document.body;
+        }
+    } else {
+        canvascontainer= document.createElement('div');
+        document.body.appendChild(canvascontainer);
+    }
+    
+    /**
+     * create a director.
+     */
+    var director = new CAAT.Director().
+            initialize(
+                width||800,
+                height||600,
+                isCanvas?canvascontainer:undefined)
+            ;
+
+    if ( !isCanvas ) {
+        canvascontainer.appendChild( director.canvas );
+    }
+
+    /**
+     * Load splash images. It is supossed the splash has some images.
+     */
+    new CAAT.ImagePreloader().loadImages(
+        imagesURL,
+        function on_load( counter, images ) {
+
+            if ( counter==images.length ) {
+
+                director.emptyScenes();
+                director.setImagesCache(images);
+
+                onEndLoading(director);
+
+                /**
+                 * Change this sentence's parameters to play with different entering-scene
+                 * curtains.
+                 * just perform a director.setScene(0) to play first director's scene.
+                 */
+                director.easeIn(
+                        0,
+                        CAAT.Scene.prototype.EASE_SCALE,
+                        2000,
+                        false,
+                        CAAT.Actor.prototype.ANCHOR_CENTER,
+                        new CAAT.Interpolator().createElasticOutInterpolator(2.5, .4) );
+
+                CAAT.loop(60);
+
+            }
+        }
+    );
+};
+
+function MapRenderer(director) {
+	var selectedField = null;
+	this.director = director;
+	this.map = null;
+	var images = {};
+	images.water = new CAAT.SpriteImage().initialize(director.getImage('water'),1,1);
+	images.grasland = new CAAT.SpriteImage().initialize(director.getImage('grasland'),1,1);
+
+	this.loadMap = function(map) {
+		this.map = map;
+	}
+
+	var setFieldStyle = function(actor, field) {
+
+		var imagimageeName = null;
+
+		switch(field.type) {
+			case 0:
+			{
+				image= images.water;
+				break;
+			}
+			case 1:
+			{
+				image= images.grasland;
+				break;
+			}
+			default:
+			{
+				image= images.water;
+				break;
+			}
+		}
+
+		actor.setBackgroundImage(image.getRef(),true);
+	}
+
+	/**
+	* function to select on-screen actors.
+	* @param e
+	*/
+	var mouseClick= function( e ) {
+		if ( selectedField ) {
+			selectedField.setAlpha(1);
+		}
+		this.setAlpha(.5);
+		selectedField = this;
+	};
+
+	this.drawMap = function(node) {
+		var fieldLength = 50;
+		var fieldOffset = 5;
+
+	        var background = new CAAT.ShapeActor().
+			setShape(CAAT.ShapeActor.prototype.SHAPE_RECTANGLE).
+			setLocation(0,0).
+			setSize(500,500).
+			setFillStyle('rgb(0,0,0)');
+		node.addChild(background);
+
+		for(var y = 0; y < this.map.fields.length; y++) {
+			var fieldRow = this.map.fields[y];
+			for(var x = 0; x < fieldRow.length; x++) {
+				var field = fieldRow[x];
+
+				var fieldActor = new CAAT.Actor().
+					setLocation(x*fieldLength + (x+1)*fieldOffset, y*fieldLength + (y+1)*fieldOffset).
+					setSize(fieldLength, fieldLength);
+				fieldActor.mouseClick = mouseClick;
+				setFieldStyle(fieldActor, field);
+
+				background.addChild(fieldActor);
+			}
+		}
+
+	};
+
+	return this;
+}
 
 var map2DFramework = function(container){
 	
@@ -66,7 +246,7 @@ var map2DFramework = function(container){
 	this.drawMap = function(){
 		this.clearLayer(0);
 		this.clearLayer(1);
-		var fields = mapData.data;
+		var fields = mapData.fields;
 		var field,xLength,yLength,unitsLength;
 		var x;
 		xLength = fields.length;
@@ -88,7 +268,7 @@ var map2DFramework = function(container){
 	
 	this.drawBasicField = function(x,y){
 		var oldFillStyle = map[0].fillStyle;
-		switch( mapData.data[x][y].type ){
+		switch( mapData.fields[x][y].type ){
 			case 0:
 				map[0].fillStyle = 'blue';
 				break;
@@ -108,7 +288,7 @@ var map2DFramework = function(container){
 	
 	this.drawImageField = function(x,y){
 		var image;
-		switch( mapData.data[x][y].type ){
+		switch( mapData.fields[x][y].type ){
 			case 0:
 				image = images.fields.water;
 				break;
