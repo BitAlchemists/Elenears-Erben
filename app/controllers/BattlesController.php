3fc95fc060347f93b1f5313a3653b025d1189c0a
Tommi Enenkel <tommienenkel@hotmail.com>
2012-02-12 22:57:35 +0100

    #79

--- app/controllers/BattlesController.php
+++ app/controllers/BattlesController.php
@@ -24,27 +24,32 @@ class BattlesController extends \lithium\action\Controller {
 		
 		$log .= "time: ".$time."<br/><br/>";
 		
-		$troopAtt = array( 
-			'count' => $party1,
-			'name' => 'Army A' );
-			
-		$troopDeff = array(
-			'count' => $party2,
-			'name' => 'Army Z' ); //todo: change name to unit type
-		
-		$armyAtt = array( 'troops' => array($troopAtt) );
-		$armyDeff = array( 'troops' => array($troopDeff) );
+		$armyAtt = array( 
+			'troops' => array(
+				array( 
+					'count' => $party1,
+					'name' => 'Army A' )
+				)
+			);
+
+		$armyDeff = array( 
+			'troops' => array(
+				array(
+					'count' => $party2,
+					'name' => 'Army Z'
+				)
+			); //todo: change name to unit type) );
 		
 		//1 - Preconditions			
 		//1.1 - Calculate Battle Points
 		foreach ($armyAtt['troops'] as $troop) {
-			$troop['battlePoints'] = $troop['count'];
-			$log .= $troop['name']." - BP: ".$troop['battlePoints']."<br/>";
+			$troop['bp'] = $troop['count'];
+			$log .= $troop['name']." - BP: ".$troop['bp']."<br/>";
 		}
 
 		foreach ($armyDeff['troops'] as $troop) {
-			$troop['battlePoints'] = $troop['count'];
-			$log .= $troop['name']." - BP: ".$troop['battlePoints']."<br/>";
+			$troop['bp'] = $troop['count'];
+			$log .= $troop['name']." - BP: ".$troop['bp']."<br/>";
 		}
 		
 		//1.2 - Base Damage Multiplier Calculation
@@ -67,10 +72,10 @@ class BattlesController extends \lithium\action\Controller {
 		//3 - Postconditions
 			
 		$log .= "<br/>End of Battle<br/>".
-			"Attacker Army: ".$troopAtt['count']." Peasants<br/>".
-			"Defender Army: ".$troopDeff['count']." Peasants<br/>";
+			"Attacker Army: ".$armyAtt['troops'][0]['count']." Peasants<br/>".
+			"Defender Army: ".$armyDeff['troops'][0]['count']." Peasants<br/>";
 		
-		return log;
+
 	}
 
 	function _rand() {
@@ -80,55 +85,49 @@ class BattlesController extends \lithium\action\Controller {
 	function _encounter($armyAtt, $armyDeff)
 	{
 		//Todo: add multiple maneuvers if BP are left
-		
 
-
-		List<TroopBattleData> attackingTroops = new List<TroopBattleData>();
-		List<TroopBattleData> defendingTroops = new List<TroopBattleData>();
+		//simulate AI move by manually assigning troops		
+		$maneuvers = array(
+			array('at' => $armyAtt['troops'][0], 'dt' => $armyDeff['troops'][0]),
+			array('at' => $armyDeff['troops'][0], 'dt' => $armyAtt['troops'][0])
+		);
 		
-		string log = "";
-		
-		//2.1 Pre-Maneuver
-		//2.1.1 Maneuver-Selection
-		armyAtt.Troops[0].OpposingTroop = armyDeff.Troops[0];
-		armyDeff.Troops[0].OpposingTroop = armyAtt.Troops[0];
-		
-		attackingTroops.Add(armyAtt.Troops[0]);
-		attackingTroops.Add(armyDeff.Troops[0]);
-		
-		defendingTroops.Add(armyAtt.Troops[0]);
-		defendingTroops.Add(armyDeff.Troops[0]);
-		
+		$log = "";
+				
 		//2.2 Maneuvers
-		foreach (TroopBattleData deff in defendingTroops) {
+		foreach ($maneuvers as $maneuver) {
 			//2.2.1
-			deff.LifePoints = deff.Troop.UnitCount;
-			log += deff.Name + " - LP: " + deff.LifePoints + Environment.NewLine;
+			$maneuver['dt']['lp'] = $maneuver['dt']['count'];
+			$log .= $maneuver['dt']['name']." - LP: ".$maneuver['dt']['lp']."<br/>";
 		}
 			
-		foreach (TroopBattleData att in attackingTroops) {
-			log += att.Name + " attacking " + att.OpposingTroop.Name + Environment.NewLine;
+		foreach ($maneuvers as $maneuver) {
+			$at = $maneuver['at'];
+			$dt = $maneuver['dt'];			
+
+			$log .= $at['name']." attacking ".$dt['name']."<br/>";
 			
-			double luck = 0.95 + rand.NextDouble() * 0.1;
-			log += att.Name + " - luck: " + luck + Environment.NewLine;
+			$luck = 0.95 + $this->_rand * 0.1;
+			$log .= $at['name']." - luck: ".$luck."<br/>";
 			//2.2.2
-			att.EffectiveDamageMultiplier = att.BaseDamageMultiplier * time * luck;
-			log += att.Name + " - EDM: " + att.EffectiveDamageMultiplier + Environment.NewLine;
+			$at['edm'] = $at['bdm'] * $time * $luck;
+			$log .= $at['name']." - EDM: ".$at['edm']."<br/>";
 			//2.2.3
-			att.PotentialDamagePoints = att.BattlePoints * att.EffectiveDamageMultiplier;
-			log += att.Name + " - PDP: " + att.PotentialDamagePoints + Environment.NewLine;
+			$at['pdp'] = $at['bp'] * $at['edm'];
+			$log .= $at['name']." - PDP: " + att.PotentialDamagePoints + Environment.NewLine;
 			//2.2.4 obsolete
+			$at['pv']
 			att.ProportionValue = att.BattlePoints / att.PotentialDamagePoints;
 			//log += deff.Name + " - PV: " + att.EffectiveDamageMultiplier + Environment.NewLine;
 			//2.2.5
 			att.OpposingTroop.DamagePoints = Math.Min(att.OpposingTroop.LifePoints, att.PotentialDamagePoints);
-			log += att.Name + " - DP: " + att.DamagePoints + Environment.NewLine;
+			log += $at['name']." - DP: " + att.DamagePoints + Environment.NewLine;
 			//2.2.6
 			att.PotentialDamagePoints -= att.OpposingTroop.DamagePoints;
-			log += att.Name + " - remaining PDP: " + att.PotentialDamagePoints + Environment.NewLine;
+			log += $at['name']." - remaining PDP: " + att.PotentialDamagePoints + Environment.NewLine;
 			//2.2.7
 			att.BattlePoints = att.PotentialDamagePoints / att.EffectiveDamageMultiplier;
-			log += att.Name + " - remaining BP: " + att.BattlePoints + Environment.NewLine;
+			log += $at['name']." - remaining BP: " + att.BattlePoints + Environment.NewLine;
 		}
 		
 		//2.3 - Post-Maneuver
@@ -136,10 +135,10 @@ class BattlesController extends \lithium\action\Controller {
 			double proportionValue = deff.DamagePoints / deff.LifePoints;
 			//2.3.1
 			deff.Troop.UnitCount -= (int)deff.DamagePoints;
-			log += deff.Name + " - remaining Peasants: " + deff.Troop.UnitCount + Environment.NewLine;
+			log += $dt['name']." - remaining Peasants: " + deff.Troop.UnitCount + Environment.NewLine;
 			//2.3.2
 			deff.BattlePoints -= deff.BattlePoints * proportionValue;
-			log += deff.Name = " - remafffffffasdfining BP: " + deff.BattlePoints + Environment.NewLine;
+			log += $dt['name']." - remafffffffasdfining BP: " + deff.BattlePoints + Environment.NewLine;
 		}
 			
 		return log;
